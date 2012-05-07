c6d7dc0ab838e163bb7c3ce75fdeb4c692105705
Ken Barber <ken@bob.sh>
2012-05-07 17:28:20 +0100

    (#14345) Added coverage for puppet/util/interactive_shell
    This is just some basic initial coverage for puppet/util/interactive_shell.
    
    I've tried to cover the basics here, and added proper handling for 'command not
    found' cases in the console.
    
    I've added some rdocs where possible as I've figured out what each piece does
    as well.

--- Rakefile
+++ Rakefile
@@ -7,9 +7,3 @@ desc 'Run RSpec'
 RSpec::Core::RakeTask.new(:spec) do |t|
   t.pattern = 'spec/{unit}/**/*.rb'
 end
-
-desc 'Generate code coverage'
-RSpec::Core::RakeTask.new(:coverage) do |t|
-  t.rcov = true
-  t.rcov_opts = ['--exclude', 'spec']
-end
--- spec/unit/puppet/util/interactive_shell_spec.rb
+++ spec/unit/puppet/util/interactive_shell_spec.rb
@@ -4,5 +4,50 @@ require 'spec_helper'
 require 'puppet/util/interactive_shell'
 
 describe Puppet::Util::InteractiveShell do
-  it 'should have some tests ...'
+  it 'should initialize with no arguments' do
+    subject
+  end
+
+  context '#get_command' do
+    it 'should read commands from stdin' do
+      $stdin.expects(:readline).returns('foo')
+      subject.get_command.should == 'foo'
+    end
+
+    it 'should return quit when EOF reached' do
+      $stdin.expects(:readline).raises(EOFError)
+      subject.get_command.should == 'quit'
+    end
+  end
+
+  context '#interact' do
+    it 'should have tests, but hard to test a loop like this'
+  end
+
+  context '#prompt' do
+    it 'should return a basic prompt at initial context' do
+      subject.expects(:print).with('/ > ')
+      subject.prompt
+    end
+
+    it 'should print a different prompt based on context' do
+      subject.context.cwd = ['package']
+      subject.expects(:print).with('/package > ')
+      subject.prompt
+    end
+  end
+
+  context '#execute_shell_action' do
+    it 'should execute the desired face action based on command' do
+      Puppet::Face[:shell, '0.0.1'].expects(:ls).with('package',
+        {:context => subject.context})
+      subject.execute_shell_action('ls package')
+    end
+
+    it 'should return a meaningful console error if command does not exist' do
+      Puppet::Face.expects(:find_action).with(:shell, 'foo')
+      subject.expects(:puts).with('command not found: foo')
+      subject.execute_shell_action('foo')
+    end
+  end
 end
